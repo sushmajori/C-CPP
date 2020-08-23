8432a1ecc933508e8841ef368d3af22179a55481
Sushama Jori <46436054+sushmajori@users.noreply.github.com>
2020-08-23 23:45:12 +0530

    Add files via upload

---
+++ fibbonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int n, num1=0,num2=1, sum;
+    cout<<"Enter Number:";
+    cin>>n;
+    cout<<num1<<" "<<num2;
+        for(int i=2;i<n;i++)
+        {
+                sum= num1+num2;
+                cout<<" "<<sum;
+
+                num1=num2;
+                num2=sum;
+
+
+        }
+           
+
+    return 0;
+}
