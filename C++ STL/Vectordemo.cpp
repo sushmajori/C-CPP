8a68b6127a21476b093c3af461d6244d8649f0e0
Sushama Jori <46436054+sushmajori@users.noreply.github.com>
2021-07-14 11:42:57 +0530

    Update Vectordemo.cpp

--- C++ STL/Vectordemo.cpp
+++ C++ STL/Vectordemo.cpp
@@ -3,27 +3,35 @@
 using namespace std;
 int main()
 {
-    vector <int> v;
-    vector <int>::iterator itr;
-
-    // for(int x : v)
-    // {
-    //     cout<<x;
-    // }
-
-    // for(itr = v.begin(); itr!=v.end(); itr++)
-    // {
-    //     cout<<*itr;
-    // }
+    vector <int> v = {1,2,3,4,5};
+    vector <int> :: iterator itr;
+    v.push_back(6);
+    v.pop_back();
 
 
-    for(int i = 0; i<=5; i++)
+    // Display the value of vector using for each loop
+    cout<<"Display the value of vector using for each loop"<<endl;
+    for(int x: v)
     {
-        v.push_back(i);
+        cout<<x<<endl;
     }
 
-    for (auto i = v.begin(); i != v.end(); ++i)
-        cout << *i << " ";
+    // Display value of vector using for loop
+    cout<<"Display value of vector using for loop"<<endl;
+    for(auto i = v.begin(); i!=v.end();i++)
+    {
+        cout<<*i<<endl;
+    }
+    // Display value of vector using iterator
+    cout<<"Display value of vector using iteraor"<<endl;
+    for(itr = v.begin(); itr!=v.end(); itr++)
+    {
+        cout<<*itr<<endl;
+    }
+    for(int i =0; i<5; i++)
+    {
+        
+    }
 
     return 0;
 }
